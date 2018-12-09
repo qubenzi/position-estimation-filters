bbedf67e9561610e1ab8fff324be81cbfb440664
Willist1 <37710114+Willist1@users.noreply.github.com>
2018-12-09 16:18:01 +0100

    Update Complementary.m

--- Complementary.m
+++ Complementary.m
@@ -2,34 +2,48 @@
 % należy usunąć macierz M
 % Aby ponownie przeliczyć macierz kątów po filtracji należy usunąć macierz
 % fi
-% Aby zmodyfikować parametry filtru komplementarnego należy dla każdej 
-% osi [x y z] zmodyfikować zawartość wektora T zawierającego stałe czasowe 
-% dla poszczególnych osi.
 % uwaga: z powodu braku magnetometru nie jest możliwym ustalenie kąta
 % obrotu względem osi pionowej(z), ponieważ nie wiadomo gdzie się znajduje
 % północ. Dlatego w celu pokazania działania filtru dla tej osi
 % potraktowaną ją jak oś x.
-
-T = [0.01 0.01 0.01];
+% zmienna ver służy do przełączania źródła danych (1 dla Magwicka, 0 dla
+% Navchip)
+clear all % zakomentuj jeśli nie chcesz przeładowania danych
+T = [0.1 0.1];
+ver = 1;
 % T = p*dt/(1-p)
 g = 10;
-ex = exist('M','var');
-if ex==0
-    M = csvread('navchip.csv',1,1);    
-    dt = 4*60*60/length(M);
-    fiAcc = [atan2(M(:,5),M(:,6)) asin(-M(:,4)/g) atan2(M(:,4),M(:,5))]*180/pi;
-    fiGyr = zeros(length(M),3);
-    fiGyr(1,:) = dt*M(1,1:3);
-    for i=2:length(M)
-        fiGyr(i,:) = fiGyr(i-1,:) +dt*M(i,1:3);
+ex = exist('DATA','var');
+if ex==0 && ver==0
+    load('DATA.mat')    
+    dt = 4*60*60/length(DATA);
+    fiAcc = [atan2(DATA(:,5),DATA(:,6)) asin(-DATA(:,4)/g)]*180/pi;
+    fiGyr = zeros(length(DATA),2);
+    fiGyr(1,:) = dt*DATA(1,1:2);
+    for i=2:length(DATA)
+        fiGyr(i,:) = fiGyr(i-1,:) +dt*DATA(i,1:2);
     end
+    len = length(DATA);
+    time = dt:dt:4*60*60;
+end
+
+ex = exist('Accelerometer','var');
+if ex==0 && ver==1
+    load('RevolvingIMU.mat')
+    dt = time(2:end)-time(1:end-1);
+    fiAcc = [atan2(Accelerometer(:,2),Accelerometer(:,3)) asin(-Accelerometer(:,1)/g)]*180/pi;
+    fiGyr = zeros(length(Gyroscope),2);
+    fiGyr(1,:) = dt(1)*Gyroscope(1,1:2);
+    for i=2:length(Gyroscope)
+        fiGyr(i,:) = fiGyr(i-1,:) +dt(i-1)*Gyroscope(i,1:2);
+    end
+    len = length(Accelerometer);
 end
 
 ex = exist('fi','var');
 if ex==0
-    len = length(M);
-    alfa = T./(dt +T)
-    fi = zeros(len,3);
+    alfa = T./(mean(dt) +T)
+    fi = zeros(len,2);
     fi(1,:) = (1-alfa).*fiAcc(1,:) +alfa.*fiGyr(1,:);
     for i=2:len
         fi(i,:) = alfa.*fi(i-1,:) + (1-alfa).*fiAcc(i,:) +alfa.*(fiGyr(i,:)-fiGyr(i-1,:));
@@ -37,49 +51,16 @@ if ex==0
 end
 
 figure
-subplot(3,1,1)
+subplot(2,1,1)
 title('Położenie obiektu obrót względem osi X')
 hold on
-plot(dt:dt:4*60*60,fiGyr(:,1))
-plot(dt:dt:4*60*60,fiAcc(:,1))
-plot(dt:dt:4*60*60,fi(:,1))
-legend('kąt wg. żyroskopu','kąt wg. akcelerometru','kąt po filtracji')
-subplot(3,1,2)
+plot(time,fiGyr(:,1))
+plot(time,fiAcc(:,1))
+plot(time,fi(:,1))
+legend('kąt wg. żyroskopu','kąt wg. akcelerometru i magnetometru','kąt po filtracji')
+subplot(2,1,2)
 title('Położenie obiektu obrót względem osi Y')
 hold on
-plot(dt:dt:4*60*60,fiGyr(:,2))
-plot(dt:dt:4*60*60,fiAcc(:,2))
-plot(dt:dt:4*60*60,fi(:,2))
-subplot(3,1,3)
-title('Położenie obiektu obrót względem osi Z (Traktowany jak oś X)')
-hold on
-plot(dt:dt:4*60*60,fiGyr(:,3))
-plot(dt:dt:4*60*60,fiAcc(:,3))
-plot(dt:dt:4*60*60,fi(:,3))
-
-% figure
-% title('Położenie po filtracji')
-% subplot(3,1,1)
-% plot(dt:dt:4*60*60,fi(:,1))
-% subplot(3,1,2)
-% plot(dt:dt:4*60*60,fi(:,2))
-% subplot(3,1,3)
-% plot(dt:dt:4*60*60,fi(:,3))
-% 
-% figure
-% title('Położenie według akcelerometru')
-% subplot(3,1,1)
-% plot(dt:dt:4*60*60,fiAcc(:,1))
-% subplot(3,1,2)
-% plot(dt:dt:4*60*60,fiAcc(:,2))
-% subplot(3,1,3)
-% plot(dt:dt:4*60*60,fiAcc(:,3))
-% 
-% figure
-% title('Położenie według żyroskopu')
-% subplot(3,1,1)
-% plot(dt:dt:4*60*60,fiGyr(:,1))
-% subplot(3,1,2)
-% plot(dt:dt:4*60*60,fiGyr(:,2))
-% subplot(3,1,3)
-% plot(dt:dt:4*60*60,fiGyr(:,3))
+plot(time,fiGyr(:,2))
+plot(time,fiAcc(:,2))
+plot(time,fi(:,2))
